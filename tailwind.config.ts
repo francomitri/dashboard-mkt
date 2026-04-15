838ebf25715e4b6bcdc6c3ee8b48ea95f6fce4a7
francomitri <mitrimkt@gmail.com>
2026-04-15 02:04:09 -0300

    subida inicial del dashboard

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
